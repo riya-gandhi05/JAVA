1dd09b9e5a9114be8987dac6240f82f367ee19a5
riya-gandhi05 <riyagandhi5551@gmail.com>
2026-02-07 19:01:11 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class JavaBasics{
+    public static void main(String[] args) {
+//            System.out.println("hello world");
+//            System.out.println("****");
+//            System.out.println("***");
+//            System.out.println("**");
+//            System.out.println("*");
+//            System.out.print("****\n***\n**\n*");
+
+//        int a=10;
+//        int b=5;
+//        int sum=a+b;
+//        System.out.println(sum);
+//        System.out.println(a+b);
+//        System.out.println(a*b);
+//        String name="deepak";
+//        System.out.println(name);
+//        float c=17;
+//        System.out.println(c);
+
+//       Scanner sc=new Scanner(System.in);
+//       int a=sc.nextInt();
+//       int b=sc.nextInt();
+//       int product = a*b;
+//       System.out.println(product);
+
+//        Scanner sc = new Scanner(System.in);
+//        float R = sc.nextFloat();
+//        System.out.println("Radius : "+R);
+//        float Area = 3.14f*R*R;
+//        System.out.println("Area of circle : "+Area);
+//
+        int $ =23;
+        System.out.println($);
+
+
+
+
+    }
+}
