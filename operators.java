5a0c4e6545589113ddc4e5038e430d8f8b12d8d7
riya-gandhi05 <riyagandhi5551@gmail.com>
2026-02-09 09:06:18 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class operators {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a number : ");
+//        int a = sc.nextInt();
+//        System.out.print("Enter a number : ");
+//        int b = sc.nextInt();
+//        int sum = a+b;
+//        int sub = a-b;
+//        int mul = a*b;
+//        int div = a/b;
+//        int mod = a%b;
+//        System.out.println(sum);
+//        System.out.println(sub);
+//        System.out.println(mul);
+//        System.out.println(div);
+//        System.out.println(mod);
+        //a++;
+//        --a;
+//        System.out.println(a);
+//        int x = 2, y = 5;
+//        int exp1 = (x * y / x);
+//        double exp2 = (double)(x * (y / x));
+//        System.out.println(exp1);
+//        System.out.print(exp2);
+
+        int x = 9, y = 12;
+        int a = 2, b = 4, c = 6;
+        int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+        System.out.println(exp);
+
+
+
+    }
+}
