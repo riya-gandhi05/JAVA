936f27599bc467715be865f5a531f3e92f5c7053
riya-gandhi05 <riyagandhi5551@gmail.com>
2026-02-11 21:58:26 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,108 @@
+import java.net.SocketTimeoutException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class conditional {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter your age :");
+//        int Age = sc.nextInt();
+//        if (Age>=18){
+//            System.out.println("You are Eligible");
+//        }
+//        else {
+//            System.out.println("Not Eligible");
+//        }
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("enter the age:");
+//        int Age=sc.nextInt();
+//        int Age=19;
+//        if(Age >= 18){
+//            System.out.println("childo");
+//        }
+//        if(Age >18 && Age <25){
+//            System.out.println("Adult");
+//        }
+//        else{
+//            System.out.println("old");
+//        }
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("enter number 1:");
+//        int num1= sc.nextInt();
+//        System.out.print("enter number 2:");
+//        int num2= sc.nextInt();
+//        if(num1>=num2){
+//            System.out.println("num1 is greater:"+num1);
+//        }
+//        else{
+//            System.out.println("num2 is greater"+num2);
+//        }
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("enter number :");
+//        int num= sc.nextInt();
+//        if(num%2==0){
+//            System.out.println("num is even");
+//        }
+//        else{
+//            System.out.println("num is odd");
+//        }
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("enter income:");
+//        int income= sc.nextInt();
+////        double tax ;
+//        if(income <500000){
+//            System.out.println("0 tax");
+//        }
+//        else if(income>=500000 && income<=1000000){
+//            tax=income*(0.2);
+//            System.out.println("tax:"+tax);
+//
+//        }
+//        else{
+//            tax=income*(0.3);
+//            System.out.println("tax:"+tax);
+//        }
+//        int number =4;
+        //ternary operator
+//        String type=((number%2)==0)?"even":"odd";
+//        System.out.println(type);
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("enter number 1:");
+//        int num1= sc.nextInt();
+//        System.out.print("enter number 2:");
+//        int num2= sc.nextInt();
+//        System.out.println("choose an operator +,-,*,/,%");
+//        System.out.print("Enter the operator :");
+//        String operator=sc.next();
+//        System.out.println("the choosen operator is :"+operator);
+//        switch(operator){
+//            case "+":
+//                System.out.println(num1+num2);
+//                break;
+//            case "-":
+//                System.out.println(num1-num2);
+//                break;
+//            case "/":
+//                System.out.println(num1/num2);
+//                break;
+//            case "*":
+//                System.out.println(num1*num2);
+//                break;
+//            case "%":
+//                System.out.println(num1%num2);
+//                break;
+//            default:
+//                System.out.println("wrong operator");
+
+//        }
+
+//        Scanner sc=new Scanner(System.in);
+//        System.out.print("enter year:");
+//        int year= sc.nextInt();
+//        if (year%4==0){
+//            System.out.println("");
+//        }
+//
+
+    }
+}
